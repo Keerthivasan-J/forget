ab0a8aa2df58efd0656bfab39ab1b3afb3e89f71
keerthi vasan <keerthivasan.jayabalan@lntinfotech.com>
2021-01-09 11:15:55 +0530

    hi

---
+++ forgetpassword.ts
@@ -0,0 +1,19 @@
+import {HttpClient} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ResetPassword } from '../models/resetpassword.model';
+
+@Injectable()
+export class ForgetPassword{
+    constructor(private hc:HttpClient){
+
+    }
+    public sendMail(id:string){
+        //Change the link
+        return this.hc.get("http://localhost:56548/api/ForgetPassword?email="+id);
+    }
+
+    public savePass(d:ResetPassword){
+        //Change the link
+        return this.hc.put("http://localhost:56548/api/ForgetPassword",d);
+    }
+}
